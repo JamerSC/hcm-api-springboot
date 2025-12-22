c92f88925cb707bf6572be660ae925950f7f60dd
Jamer Catalla <mr.catalla28@gmail.com>
2025-12-22 22:13:20 +0800

    feature: implement search & filter fxn to job module

--- src/main/java/com/jamersc/springboot/hcm_api/controller/ApplicantController.java
+++ src/main/java/com/jamersc/springboot/hcm_api/controller/ApplicantController.java
@@ -10,7 +10,9 @@ import com.jamersc.springboot.hcm_api.utils.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,6 +22,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -38,8 +41,18 @@ public class ApplicantController {
     @PreAuthorize("hasAuthority('VIEW_OPEN_JOBS')")
     @GetMapping("/open/jobs")
     public ResponseEntity<ApiResponse<Page<JobResponseDto>>> getOpenJobs(
-            @PageableDefault(page = 0, size = 10, sort = "title") Pageable pageable) {
-        Page<JobResponseDto> retrievedOpenJobs = jobService.getOpenJobs(pageable);
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateFrom,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateTo,
+            @PageableDefault(page = 0, size = 10, sort = "postedDate", direction = Sort.Direction.DESC) Pageable pageable)
+    {
+        Page<JobResponseDto> retrievedOpenJobs = jobService.getOpenJobs(
+                search,
+                dateFrom,
+                dateTo,
+                pageable
+        );
+
         ApiResponse<Page<JobResponseDto>> response = ApiResponse.<Page<JobResponseDto>>builder()
                 .success(true)
                 .message("List of open jobs retrieved successfully!")
--- src/main/java/com/jamersc/springboot/hcm_api/repository/JobSpecification.java
+++ src/main/java/com/jamersc/springboot/hcm_api/repository/JobSpecification.java
@@ -1,4 +1,129 @@
 package com.jamersc.springboot.hcm_api.repository;
 
+import com.jamersc.springboot.hcm_api.entity.Job;
+import com.jamersc.springboot.hcm_api.entity.JobStatus;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JobSpecification {
+
+    /**
+     * For Manager/Admin
+     */
+    public static Specification<Job> getJobs(
+            String search,
+            Long departmentId,
+            JobStatus status,
+            LocalDate dateFrom,
+            LocalDate dateTo
+    ) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            // 1. Search Logic (Title or Description)
+            if (StringUtils.hasText(search)) {
+                String searchPattern = "%" + search.toLowerCase() + "%";
+                Predicate titlePredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("title")), searchPattern);
+                Predicate descPredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("description")), searchPattern);
+                Predicate descLocation = criteriaBuilder.like(criteriaBuilder.lower(root.get("location")), searchPattern);
+                predicates.add(criteriaBuilder.or(titlePredicate, descPredicate, descLocation));
+            }
+
+            // 2. Department Filter (Relationship Join)
+            if (departmentId != null) {
+                // "department" matches the field name in Job entity: private Department department;
+                // "id" matches the field name in Department entity: private Long id;
+                predicates.add(criteriaBuilder.equal(root.get("department").get("id"), departmentId));
+            }
+
+            if (status != null) {
+                predicates.add(criteriaBuilder.equal(root.get("status"), status));
+            }
+
+            if (dateFrom != null) {
+                OffsetDateTime startDateTime =
+                        dateFrom.atStartOfDay()
+                                .atOffset(ZoneOffset.UTC);
+
+                predicates.add(
+                        criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"), startDateTime)
+                );
+            }
+
+            if (dateTo != null) {
+                OffsetDateTime endDateTime =
+                        dateTo.plusDays(1)
+                                .atStartOfDay()
+                                .atOffset(ZoneOffset.UTC);
+
+                predicates.add(
+                        criteriaBuilder.lessThan(root.get("createdAt"), endDateTime)
+                );
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+
+    /**
+     * For Applicants
+     */
+    public static Specification<Job> getOpenJobs(
+            String search,
+            LocalDate dateFrom,
+            LocalDate dateTo
+    ) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            predicates.add(
+                    criteriaBuilder.equal(root.get("status"), JobStatus.OPEN)
+            );
+
+
+            // 1. Search Logic (Title or Description)
+            if (StringUtils.hasText(search)) {
+                String searchPattern = "%" + search.toLowerCase() + "%";
+                Predicate titlePredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("title")), searchPattern);
+                Predicate descPredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("description")), searchPattern);
+                Predicate descLocation = criteriaBuilder.like(criteriaBuilder.lower(root.get("location")), searchPattern);
+                predicates.add(criteriaBuilder.or(titlePredicate, descPredicate, descLocation));
+            }
+
+            if (dateFrom != null) {
+                predicates.add(
+                        criteriaBuilder.greaterThanOrEqualTo(
+                                criteriaBuilder.function(
+                                        "date",
+                                        LocalDate.class,
+                                        root.get("postedDate") // OffsetDateTime column
+                                ),
+                                dateFrom
+                        )
+                );
+            }
+
+            if (dateTo != null) {
+                predicates.add(
+                        criteriaBuilder.lessThanOrEqualTo(
+                                criteriaBuilder.function(
+                                        "date",
+                                        LocalDate.class,
+                                        root.get("postedDate")
+                                ),
+                                dateTo
+                        )
+                );
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
 }
